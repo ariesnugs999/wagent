358f312f6f0d3baa0bf49e99a4f976bbc1831b41
ariesnugs999 <ariesnugs999.com>
2021-03-01 16:14:31 +0700

    x

---
+++ app/logs/log-2020-04-28.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-04-28 10:15:48 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\xampp\htdocs\wtouch\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-04-28 10:15:48 --> Unable to connect to the database
+ERROR - 2020-04-28 10:15:48 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\xampp\htdocs\wtouch\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-04-28 10:15:48 --> Severity: Error --> session_start(): Failed to initialize storage module: user (path: sessions) C:\xampp\htdocs\wtouch\system\libraries\Session\Session.php 143
